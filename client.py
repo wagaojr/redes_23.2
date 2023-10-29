219808ab05f538df09363b6ed95cb7e6e7acc05c
wagaojr <wagnerjunior@poli.ufrj.br>
2023-10-28 21:09:48 -0300

    commented client

--- client.py
+++ client.py
@@ -2,6 +2,8 @@
 
 import socket
 
+###### Definições feitas em turma ############
+
 HEADER = 64
 PORT = 18
 FORMAT = 'utf-8'
@@ -9,28 +11,41 @@ DISCONNECT_MESSAGE = ':D'
 SERVER = 'localhost'
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+##############################################
 
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Cria um socket do cliente
+
+client.connect(ADDR) #Estabelece uma conexão com o endereço
 
 def send(msg):
-    message = msg.encode(FORMAT)
+    
+    message = msg.encode(FORMAT) #Codifica a mensagem
+
     msg_len = len(message)
+
+    #Codifica o comprimento da mensagem como uma string
     send_len = str(msg_len).encode(FORMAT)
     send_len += b' ' * (HEADER - len(send_len))
+    ###
+
+    #Envia o comprimento da mensagem seguido pela mensagem em si para o servidor.
     client.send(send_len)
     client.send(message)
-    print(client.recv(2048).decode(FORMAT))
+    ###
 
+    print(client.recv(2048).decode(FORMAT)) #Aguarda uma resposta do servidor
 
 while True:
     entry = input('Sua mensagem: ')
-    send(entry)
+
+    send(entry) #Envia a mensagem ao servidor.
+
     if entry == DISCONNECT_MESSAGE:
         print("Desconectando...")
         break
 
-client.close()
+client.close() #Fecha a conexão
+
 
 
 
