7050ad2ebec9e539ecd1d0d607bd99b0810416e0
wagaojr <wagnerjunior@poli.ufrj.br>
2023-11-01 22:12:12 -0300

    refatorado: cliente passa a receber e enviar mensagens simultaneamente

--- client.py
+++ client.py
@@ -1,53 +1,50 @@
-#!/usr/bin/env python3
-
 import socket
 import threading
+import os
 
-###### Definições feitas em turma ############
+# Obtém o ID do processo atual
+id = os.getpid()
 
-HEADER = 64
-PORT = 18000
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = ':D'
-SERVER = 'localhost'
-ADDR = (SERVER, PORT)
+# Define o endereço do servidor como localhost na porta 18000
+SERVIDOR = 'localhost'
+PORTA = 18000
+FORMATO = 'utf-8'
+endereco = (SERVIDOR, PORTA)
 
-##############################################
+# Cria um soquete de cliente
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect(endereco)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Cria um socket do cliente
+# Função para enviar mensagens para o servidor
+def transmitir(string):
+    mensagem = string.encode(FORMATO)
+    tamanho_mensagem = str(len(mensagem)).encode(FORMATO)
+    tamanho_mensagem += b' ' * (64 - len(tamanho_mensagem))
+    socket.send(tamanho_mensagem)
+    socket.send(mensagem)
 
-client.connect(ADDR) #Estabelece uma conexão com o endereço
+# Função para ouvir mensagens do servidor
+def ouvir():
+    while True:
+        string = socket.recv(2048).decode(FORMATO)
+        if not string:
+            break
+        print("\n" + string)
 
-def send(msg):
+# Início da execução do programa
+if __name__ == "__main__":
+    # Inicia a função 'ouvir' em uma thread separada
+    servidor = threading.Thread(target=ouvir)
+    servidor.daemon = True
+    servidor.start()
     
-    message = msg.encode(FORMAT) #Codifica a mensagem
-
-    msg_len = len(message)
-
-    #Codifica o comprimento da mensagem como uma string
-    send_len = str(msg_len).encode(FORMAT)
-    send_len += b' ' * (HEADER - len(send_len))
-    ###
-
-    #Envia o comprimento da mensagem seguido pela mensagem em si para o servidor.
-    client.send(send_len)
-    client.send(message)
-    ###
-
-    print(client.recv(HEADER).decode(FORMAT)) #Aguarda uma resposta do servidor
-
-while True:
-    entry = input('Sua mensagem: ')
-
-    send(entry) #Envia a mensagem ao servidor.
-
-    if entry == DISCONNECT_MESSAGE:
-        print("Desconectando...")
-        break
-
-client.close() #Fecha a conexão
-
-
-
-
+    while True:
+        mensagem = input('Sua mensagem: ')
+        transmitir(mensagem)
         
+        # Se a mensagem for ':D', fecha o soquete do cliente e encerra o programa
+        if mensagem == ':D':
+            socket.close()
+            os.kill(id, 9)
+
+
